f8fa09121cf0a4fc6a428fd444feee0edb134475
Mykyta <76209725+elmalving@users.noreply.github.com>
2023-07-24 12:20:02 +0200

    Update site.js

--- js/site.js
+++ js/site.js
@@ -1,4 +1,43 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+document.getElementById("currentYear").appendChild(document.createTextNode(new Date().getFullYear()))
 
-// Write your JavaScript code.
+
+const json_path = `../json/${window.location.pathname != '/' ? window.location.pathname.split('/').pop().split('.')[0].toLowerCase() : 'index'}.json`;
+const lang_button = document.getElementById('lang-button');
+lang_button.addEventListener('click', change_language);
+
+const current_lang = get_current_language()
+if (current_lang === null) {
+    localStorage.setItem('language', 'en');
+}
+else {
+    translate_page(current_lang);
+}
+
+function get_current_language() {
+    return localStorage.getItem('language');
+}
+
+function change_language() {
+    const current_lang = get_current_language();
+    if (current_lang == 'en') {
+        translate_page('cz');
+    }
+    else {
+        translate_page('en');
+    }
+}
+
+function translate_page(language) {
+    lang_button.style.backgroundImage = `url('../img/${language}.svg')`;
+
+    fetch(json_path)
+        .then(response => response.json())
+        .then(translations => {
+            for (const item in translations[language]) {
+                document.querySelector(item).innerHTML = translations[language][item];
+            }
+        })
+        .catch(error => console.error('Error reading JSON file:', error));
+
+    localStorage.setItem('language', language);
+}
