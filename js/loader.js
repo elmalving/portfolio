a7a67e2c576fc2cc44cea284f75725280af8ddaa
Mykyta <76209725+elmalving@users.noreply.github.com>
2023-08-02 19:30:58 +0200

    Removed pre-loader

--- js/loader.js
+++
@@ -1,9 +0,0 @@
-const page = document.querySelector(".after-load");
-const pre_loader = document.querySelector(".pre-loader");
-
-window.addEventListener("load", function() {
-    setTimeout(function() {
-        page.style.display = "flex";
-        pre_loader.style.display = "none";
-    }, 1000);
-});
