0260be91caef1bb1dd8a6f6f92f2dd97de3abf32
elmalving <gromonikita@ukr.net>
2025-09-21 18:30:42 +0200

    add scrolling illusion to home page

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,3 +1,4 @@
+import { useRef } from 'react';
 import { useTranslator } from '../../contexts/translator';
 import { socialMediaIcons } from '../../assets';
 import * as styles from './Home.css';
@@ -19,39 +20,61 @@ const SocialMediaThumbnail = ({
 };
 
 export const Home = () => {
+    const textCards = useRef<HTMLElement[]>([]);
     const { translate } = useTranslator('home');
+
+    const TextCard = ({ text }: { text: string }) => (
+        <div
+            ref={(el) => {
+                if (el && !textCards.current.includes(el)) {
+                    textCards.current.push(el);
+                }
+            }}
+        >
+            {translate(text)}
+        </div>
+    );
+
+    const articleScroll = (event: React.UIEvent<HTMLDivElement>) => {
+        console.log(textCards);
+        const scrollContainer = event.currentTarget;
+
+        const containerHeight = scrollContainer.clientHeight;
+        const containerCenter = containerHeight / 2;
+        const scrollPosition = scrollContainer.scrollTop;
+
+        textCards.current.forEach((textCard) => {
+            const textCardTop = textCard.offsetTop;
+            const textCardCenter =
+                textCardTop - scrollPosition + textCard.clientHeight / 2;
+            const distanceFromCenter = Math.abs(
+                textCardCenter - containerCenter
+            );
+
+            const translateZValue = distanceFromCenter / 3;
+
+            textCard.style.transform = `translateZ(-${translateZValue}px)`;
+        });
+    };
 
     return (
         <>
             <div className={styles.theme}>
                 <h1 className={styles.display4}>{translate('Welcome')}</h1>
             </div>
-            <article className={styles.mainTextContainer}>
-                <div>
-                    {translate(
-                        'In 2020 I have completed programming courses at the highly acclaimed “Hillel IT School”. (https://ithillel.ua) The courses consisted of in-dept learning and practising Web Development {HTML, CSS, JS, mySQL, C#}, I also have a high level of proficiency in the Python programming language.'
-                    )}
-                </div>
-                <div>
-                    {translate(
-                        'Currently, I have little professional experience as a programmer working on "Social media scheduler" project at Better Plan, but I am fully ready to devote myself to enhance my skill set and deliver high-quality solutions.'
-                    )}
-                </div>
-                <div>
-                    {translate(
-                        'I embrace challenges and approach them with a growth mindset, always seeking new opportunities to broaden my expertise. With a drive for constant improvement, I am eager to take on new learning experiences and contribute to impactful projects.'
-                    )}
-                </div>
-                <div>
-                    {translate(
-                        'Sociable and have experience working in a team.'
-                    )}
-                </div>
-                <div>
-                    {translate(
-                        'Fluent in English and Czech, native in Russian and Ukrainian.'
-                    )}
-                </div>
+            <article
+                onScroll={articleScroll}
+                className={styles.mainTextContainer}
+            >
+                <TextCard text="In 2020 I have completed programming courses at the highly acclaimed “Hillel IT School”. (https://ithillel.ua) The courses consisted of in-dept learning and practising Web Development {HTML, CSS, JS, mySQL, C#}, I also have a high level of proficiency in the Python programming language." />
+
+                <TextCard text='Currently, I have little professional experience as a programmer working on "Social media scheduler" project at Better Plan, but I am fully ready to devote myself to enhance my skill set and deliver high-quality solutions.' />
+
+                <TextCard text="I embrace challenges and approach them with a growth mindset, always seeking new opportunities to broaden my expertise. With a drive for constant improvement, I am eager to take on new learning experiences and contribute to impactful projects." />
+
+                <TextCard text="Sociable and have experience working in a team." />
+
+                <TextCard text="Fluent in English and Czech, native in Russian and Ukrainian." />
             </article>
             <div className={styles.contactList}>
                 <SocialMediaThumbnail
